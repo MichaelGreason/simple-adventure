0e1f358cff8ec557eea8dc04cbd461793275a540
Michael Greason <mgreason18@gmail.com>
2023-05-15 16:58:42 -0400

    made simple counter

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Home() {
-  return <div>HI!!!!</div>;
+  let [count, setCount] = useState(0);
+
+  function addCount() {
+    setCount((count += 1));
+  }
+  function subtractCount() {
+    setCount((count -= 1));
+  }
+
+  return (
+    <>
+      <h1 className="header">Counter: {count} </h1>
+      <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button>
+    </>
+  );
 }
