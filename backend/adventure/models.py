569eac8eb2bba1a517d36e03a30c6cc6bc151dd9
Jeeglobs <dp.hudgins@hotmail.com>
2023-05-16 09:30:47 -0400

    add stats to User & Enemy Models

--- backend/adventure/models.py
+++ backend/adventure/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField()
+    hit_points = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    damage = models.IntegerField()
+
+
+class Enemy(models.Model):
+    name = models.CharField()
+    hit_points = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    damage = models.IntegerField()
