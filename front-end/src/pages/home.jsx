7bc921dca36d1841a403d17179c98e203f90c966
Michael Greason <mgreason18@gmail.com>
2023-05-17 10:27:21 -0400

    added title and avatar

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -4,19 +4,63 @@ import React from "react";
 export default function Home() {
   let [count, setCount] = useState(0);
 
-  function addCount() {
-    setCount((count += 1));
-  }
-  function subtractCount() {
-    setCount((count -= 1));
-  }
+  // function addCount() {
+  //   setCount((count += 1));
+  // }
+  // function subtractCount() {
+  //   setCount((count -= 1));
+  // }
 
   return (
     <>
       <div style={{ textAlign: "center" }}>
-        <h1>Counter: {count} </h1>
+        <h1 className="title">Simple Adventure</h1>
       </div>
       <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginLeft: "5rem",
+          marginTop: "5rem",
+        }}
+      >
+        <img
+          src="/src/temp-img/hero1.png"
+          alt="avatar"
+          className="avatar"
+          style={{
+            height: "10rem",
+            borderRadius: "50%",
+            alignSelf: "center",
+          }}
+        ></img>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyItems: "center",
+            alignItems: "center",
+          }}
+        >
+          <p
+            className="text"
+            style={{
+              marginLeft: "2rem",
+            }}
+          >
+            Name:
+          </p>
+          <p
+            className="text"
+            style={{
+              marginLeft: "2rem",
+            }}
+          >
+            Weapon:
+          </p>
+        </div>
+      </div>
+      {/* <div
         style={{
           textAlign: "center",
           display: "flex",
@@ -32,7 +76,7 @@ export default function Home() {
         >
           Subtract
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
