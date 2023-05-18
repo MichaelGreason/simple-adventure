7c08353b7ecc995e77c64c21bfa5af2a5e2b6af8
Jeeglobs <dp.hudgins@hotmail.com>
2023-05-18 12:17:20 -0400

    make Weapon Model & add weapon field to User&Enemy Models

--- backend/adventure/models.py
+++ backend/adventure/models.py
@@ -3,19 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    name = models.CharField(default='Player')
+    name = models.CharField(default='player')
     hit_points = models.IntegerField(default=0)
     attack = models.IntegerField(default=0)
     defense = models.IntegerField(default=0)
-    damage = models.IntegerField(default=0)
+    speed = models.IntegerField(default=0)
+    weapon = models.ManyToManyField(
+        to='Weapon', blank=True, related_name='user')
 
 
 class Enemy(models.Model):
-    name = models.CharField(default='Enemy')
+    name = models.CharField(default='enemy')
     hit_points = models.IntegerField(default=0)
     attack = models.IntegerField(default=0)
     defense = models.IntegerField(default=0)
-    damage = models.IntegerField(default=0)
+    speed = models.IntegerField(default=0)
+    weapon = models.ManyToManyField(
+        to='Weapon', blank=True, related_name='enemy')
 
     class Meta:
         verbose_name_plural = 'enemies'
+
+
+class Weapon(models.Model):
+    name = models.CharField(default='weapon')
+    damage = models.IntegerField(default=0)
