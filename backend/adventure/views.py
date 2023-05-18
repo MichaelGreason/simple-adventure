9d8273f66f813c6a5819e4976d7478f6547a8f8f
Jeeglobs <dp.hudgins@hotmail.com>
2023-05-18 12:27:39 -0400

    make roll_d20 function

--- backend/adventure/views.py
+++ backend/adventure/views.py
@@ -1,5 +1,8 @@
 # from django.shortcuts import render
 from django.contrib.auth.models import User, Enemy
 from rest_framework import generics
+import random
 
-# Create your views here.
+
+def roll_d20():
+    return random.randint(1, 20)
