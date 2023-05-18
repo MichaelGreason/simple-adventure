cea7f0409c0ccfd3dd80932b1d1848669cd2984a
Michael Greason <mgreason18@gmail.com>
2023-05-18 13:52:37 -0400

    installed semantic

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import Home from "./pages/home";
 import React from "react";
+import SignUp from "./pages/sign-up";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </>
   );
--- front-end/src/pages/sign-up.jsx
+++ front-end/src/pages/sign-up.jsx
@@ -2,7 +2,54 @@ import { useState } from "react";
 import React from "react";
 
 export default function SignUp() {
-  <>
-    <div></div>
-  </>;
+  return (
+    <>
+      <div>
+        <form className="pure-form pure-form-aligned">
+          <fieldset>
+            <div className="pure-control-group">
+              <label>Username</label>
+              <input type="text" id="aligned-name" placeholder="Username" />
+              <span className="pure-form-message-inline">
+                This is a required field.
+              </span>
+            </div>
+            <div className="pure-control-group">
+              <label>Password</label>
+              <input
+                type="password"
+                id="aligned-password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="pure-control-group">
+              <label>Email Address</label>
+              <input
+                type="email"
+                id="aligned-email"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="pure-control-group">
+              <label>Supercalifragilistic Label</label>
+              <input
+                type="text"
+                id="aligned-foo"
+                placeholder="Enter something here..."
+              />
+            </div>
+            <div className="pure-controls">
+              <label className="pure-checkbox">
+                <input type="checkbox" id="aligned-cb" /> I&#x27;ve read the
+                terms and conditions
+              </label>
+              <button type="submit" className="pure-button pure-button-primary">
+                Submit
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
 }
