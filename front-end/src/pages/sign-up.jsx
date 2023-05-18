c4810198ccbdaa4c81f7daf36d71b748324df0c3
Michael Greason <mgreason18@gmail.com>
2023-05-18 18:51:33 -0400

    sign up page setup complete

--- front-end/src/pages/sign-up.jsx
+++ front-end/src/pages/sign-up.jsx
@@ -4,26 +4,42 @@ import { Button, Form } from "semantic-ui-react";
 export default function SignUp() {
   return (
     <>
-      <div>
-        <h1 className="">Sign Up!</h1>
+      <div className="">
+        <div className="mt-10 ">
+          <h1 className="text-center text-2xl font-cursive">Welcome to</h1>
+        </div>
+        <div className="">
+          <h1 className="text-center text-6xl font-cursive">
+            Simple Adventure
+          </h1>
+        </div>
+        <div className="mt-20 ">
+          <h1 className="text-center text-4xl font-cursive underline">
+            Sign Up
+          </h1>
+        </div>
+        <div className="flex justify-center h-screen">
+          <Form className="font-cursive mx-5 mt-10 w-2/5 ">
+            <Form.Field>
+              <label>First Name</label>
+              <input placeholder="First Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input placeholder="Last Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Username</label>
+              <input placeholder="Username" />
+            </Form.Field>
+            <div className="text-center">
+              <Button color="black" type="submit" className="">
+                <span className="font-cursive text-lg">Submit</span>
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
-      <Form className=" mx-5 mt-20 w-2/5">
-        <Form.Field>
-          <label>First Name</label>
-          <input placeholder="First Name" />
-        </Form.Field>
-        <Form.Field>
-          <label>Last Name</label>
-          <input placeholder="Last Name" />
-        </Form.Field>
-        <Form.Field>
-          <label>Username</label>
-          <input placeholder="Username" />
-        </Form.Field>
-        <Button inverted color="blue" type="submit">
-          Submit
-        </Button>
-      </Form>
     </>
   );
 }
