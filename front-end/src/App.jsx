22886b598ced5f2f5bb9dfef96cc7e0a01dc0d05
Michael Greason <mgreason18@gmail.com>
2023-05-18 14:24:04 -0400

    tailwind installed success

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,5 @@
 import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
-import "./styles/index.css";
 import Home from "./pages/home";
 import React from "react";
 import SignUp from "./pages/sign-up";
--- front-end/src/pages/sign-up.jsx
+++ front-end/src/pages/sign-up.jsx
@@ -3,22 +3,24 @@ import { Button, Form } from "semantic-ui-react";
 
 export default function SignUp() {
   return (
-    <Form>
-      <Form.Field>
-        <label>First Name</label>
-        <input placeholder="First Name" />
-      </Form.Field>
-      <Form.Field>
-        <label>Last Name</label>
-        <input placeholder="Last Name" />
-      </Form.Field>
-      <Form.Field>
-        <label>Username</label>
-        <input placeholder="Username" />
-      </Form.Field>
-      <Button inverted color="blue" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Username</label>
+          <input placeholder="Username" />
+        </Form.Field>
+        <Button inverted color="blue" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
