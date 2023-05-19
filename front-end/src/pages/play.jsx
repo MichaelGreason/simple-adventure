aee8881c4b4e72e168e0acd751fb8d1600b12307
Michael Greason <mgreason18@gmail.com>
2023-05-19 09:48:40 -0400

    fixed roll text

--- front-end/src/pages/play.jsx
+++ front-end/src/pages/play.jsx
@@ -20,18 +20,38 @@ export default function Play() {
 
   return (
     <>
+      <div className=" flex justify-center items-center flex-col h-screen">
+        <div className="flex flex-col mb-10">
+          <div className="font-cursive text-xl">HP:</div>
+          <img
+            src="/src/temp-img/hero1.png"
+            alt="avatar"
+            className="h-48 self-center"
+          ></img>
+        </div>
+        <div className="flex items-center justify-center">
+          <img
+            onClick={rollDie}
+            className="w-1/5 cursor-pointer"
+            src="src/temp-img/pngwing.com.png"
+          />
+          <div className="text-xl font-cursive">
+            {roll === 11 ? `You rolled an ${roll}` : `You rolled a ${roll}`}{" "}
+          </div>
+        </div>
+        <div className="flex flex-col mb-10">
+          <img
+            src="/src/temp-img/hero1.png"
+            alt="avatar"
+            className="mt-10 h-48 self-center"
+          ></img>
+          <div className="font-cursive text-xl">HP:</div>
+        </div>
+      </div>
       <div className="absolute bottom-5 left-5">
         <Button color="black" onClick={handleQuit}>
           <span className="font-cursive">QUIT</span>
         </Button>
-      </div>
-      <div className=" flex justify-center items-center flex-col h-screen">
-        <img
-          onClick={rollDie}
-          className="w-1/5 cursor-pointer"
-          src="src/temp-img/pngwing.com.png"
-        />
-        <div className="text-xl">You rolled a {roll}</div>
       </div>
     </>
   );
