bc9be671721b470c741efb9df32b005b5884f613
Jeeglobs <dp.hudgins@hotmail.com>
2023-05-19 17:27:12 -0400

    modify roll_d20 and determine_initiative

--- backend/adventure/views.py
+++ backend/adventure/views.py
@@ -12,24 +12,14 @@ class UserProfile(generics.RetrieveUpdateDestroyAPIView):
         return self.request.user
 
 
-def player_roll_d20():
-    player_roll = random.randint(1, 20)
-    return player_roll
-
-
-def enemy_roll_d20():
-    enemy_roll = random.randint(1, 20)
-    return enemy_roll
+def roll_d20():
+    roll = random.randint(1, 20)
+    return roll
 
 
 def determine_initiative():
-    # player_roll_d20() == player_roll
-    # player_speed = player_roll + User.speed
-    player_speed = User.speed + player_roll_d20()
-
-    # enemy_roll_d20() == enemy_roll
-    # enemy_speed = enemy_roll + Enemy.speed
-    enemy_speed = Enemy.speed + enemy_roll_d20()
+    player_speed = User.speed + User.weapon.speed + roll_d20()
+    enemy_speed = Enemy.speed + Enemy.weapon.speed + roll_d20()
 
     if player_speed > enemy_speed:
         pass
