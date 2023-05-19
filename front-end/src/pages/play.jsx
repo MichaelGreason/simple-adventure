c2a14d225916a290525a2a7edb09bcd98e5275c5
Michael Greason <mgreason18@gmail.com>
2023-05-19 00:07:38 -0400

    made a custom die

--- front-end/src/pages/play.jsx
+++ front-end/src/pages/play.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import axios from "axios";
 import { Button } from "semantic-ui-react";
+import _ from "lodash";
 
 export default function Play() {
   function rollDie() {
-    const options = {
-      method: "GET",
-      url: "https://dice-roll-simulator.p.rapidapi.com/custom-die",
-      params: { sides: "20" },
-      headers: {
-        "X-RapidAPI-Key": "e66b370ec4msh98f67ac18fcb312p13c236jsna6a87d6a9095",
-        "X-RapidAPI-Host": "dice-roll-simulator.p.rapidapi.com",
-      },
-    };
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data); // Handle the response data
-      })
-      .catch(function (error) {
-        console.error(error); // Handle any errors
-      });
+    const result = _.random(1, 20);
+    console.log(result);
   }
 
   return (
@@ -31,6 +17,7 @@ export default function Play() {
           className="w-1/5 cursor-pointer"
           src="src/temp-img/pngwing.com.png"
         />
+        You rolled a
       </div>
     </>
   );
