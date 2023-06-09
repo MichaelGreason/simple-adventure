5dace62d7c315a39ce77d9d7bf642b8c162135a2
Jeeglobs <dp.hudgins@hotmail.com>
2023-06-09 14:00:34 -0400

    add name field back

--- backend/adventure/models.py
+++ backend/adventure/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
+    name = models.Charfield(default='player')
     hit_points = models.IntegerField(default=0)
     attack = models.IntegerField(default=0)
     defense = models.IntegerField(default=0)
