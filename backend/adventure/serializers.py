55e6335bea86759316e3663ae4e32a670bff5f31
Jeeglobs <dp.hudgins@hotmail.com>
2023-06-11 11:11:59 -0400

    fix UserSerializer

--- backend/adventure/serializers.py
+++ backend/adventure/serializers.py
@@ -2,26 +2,6 @@ from rest_framework import serializers
 from .models import User, Enemy, Weapon
 
 
-class UserSerializer(serializers.ModelSerializer):
-    weapon_name = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = User
-        fields = (
-            'username',
-            'name',
-            'hit_points',
-            'attack',
-            'defense',
-            'speed',
-            'weapon',
-            'weapon_name',
-            'skill_points',
-            'kills',
-            'deaths',
-        )
-
-
 class EnemySerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -34,3 +14,23 @@ class WeaponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weapon
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    weapon = WeaponSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'name',
+            'hit_points',
+            'attack',
+            'defense',
+            'speed',
+            'weapon',
+            'skill_points',
+            'kills',
+            'deaths',
+        )
+        # fields = '__all__'
