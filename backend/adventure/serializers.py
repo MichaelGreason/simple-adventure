e497aa79d81717e2ebd9b168c57fcf1e352af54f
Michael Greason <mgreason18@gmail.com>
2023-06-14 14:39:33 -0400

    got nowhere

--- backend/adventure/serializers.py
+++ backend/adventure/serializers.py
@@ -27,17 +27,7 @@ class UserSerializer(serializers.ModelSerializer):
             'kills',
             'deaths',
         )
-
-    # def update(self, instance, validated_data):
-    #     weapon_data = validated_data.pop('weapon', None)
-
-    #     if weapon_data is not None:
-    #         weapon_serializer = WeaponSerializer(
-    #             instance.weapon, data=weapon_data)
-    #         weapon_serializer.is_valid(raise_exception=True)
-    #         weapon_serializer.save()
-
-    #     return super().update(instance, validated_data)
+    
 
 
 class EnemySerializer(serializers.ModelSerializer):
